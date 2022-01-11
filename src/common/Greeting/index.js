0dae080c67de3f191c85aabb1698f6e5e90e4fe6
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-11 23:40:19 +0100

    created tile and add style

--- src/common/Greeting/index.js
+++ src/common/Greeting/index.js
@@ -1,9 +1,32 @@
+import { BasicInfo, Button, Content, EnvelopeIcon, Image, Mail, Name, Tile, Welcom } from "./styled";
+
 const Greeting = () => {
-    return (
-        <p>
-            lore
-        </p>
-    );
+  return (
+    <Tile>
+      <Image />
+      <Content>
+        <Welcom>
+          This is
+        </Welcom>
+        <Name>
+          Olaf Tarkowski
+        </Name>
+        <BasicInfo>
+          👨🏻‍💻 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+        </BasicInfo>
+        <Mail href="mailto:tarkowski.software@gmail.com">
+          <Button>
+            <EnvelopeIcon />
+            Hire Me
+          </Button>
+        </Mail>
+      </Content>
+      <p>
+        Button
+      </p>
+    </Tile >
+
+  );
 };
 
 export default Greeting;
---
+++ src/common/Greeting/styled.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+import picture from "../../images/narty_narty.jpg"
+import { ReactComponent as Envelope } from "../../images/envelope.svg";
+
+export const Tile = styled.div`
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    grid-gap: 66px;
+`;
+
+export const Image = styled.div`
+    width: 398px;
+    height: 398px;
+    background-position: center;
+    background-size: cover;
+    background-image: url(${picture});
+    border-radius: 50% ;
+`;
+
+export const Content = styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+export const Welcom = styled.div`
+    margin-bottom: 12px;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 130%;
+    text-transform: uppercase;
+    color: ${({theme}) => theme.color.slateGray};
+`;
+
+export const Name = styled.div`
+    margin-bottom: 35px;
+    font-weight: 900;
+    font-size: 38px;
+    line-height: 46px;
+`;
+
+export const BasicInfo = styled.div`
+    margin-bottom: 32px;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 140%;
+    color: ${({theme}) => theme.color.slateGray};
+`;
+
+export const Mail = styled.a`
+    text-decoration: none;
+`;
+
+export const Button = styled.button`
+    padding:12px 16px; 
+    border: none;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 24px;
+    color: ${({theme}) => theme.color.white};
+    background-color: ${({theme}) => theme.color.indigo};
+`;
+
+export const EnvelopeIcon = styled(Envelope)`
+    margin-right:14px;
+`;
