5a03536a13fc1297ebfc7fce2c59768668ed567e
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-11 21:10:02 +0100

    Swapped Skills and Learn into a much better idea

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import Greeting from "./features/Greeting";
-import Learn from "./features/Learn";
-import Skills from "./features/Skills";
+import Greeting from "./common/Greeting";
+import Tiles from "./common/Tiles";
+import skillList from "./common/Tiles/Data/skillList";
+import targetList from "./common/Tiles/Data/targetList";
 import { StyledPage } from "./styled";
 
 function App() {
   return (
     <StyledPage>
       <Greeting />
-      <Skills />
-      <Learn />
+      <Tiles title={"My skillset includes"} data={skillList} />
+      <Tiles title={"What I want to learn next"} data={targetList} />
     </StyledPage>
   );
 };
