b21f9d72f4976eb8a54821342b20692f1b8bee41
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-12 22:16:51 +0100

    Add Portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Footer from "./common/Footer";
 import Greeting from "./common/Greeting";
+import Portfolio from "./common/Portfolio";
 import Tiles from "./common/Tiles";
 import skillList from "./common/Tiles/Data/skillList";
 import targetList from "./common/Tiles/Data/targetList";
@@ -11,6 +12,7 @@ function App() {
       <Greeting />
       <Tiles title={"My skillset includes 🛠️"} data={skillList} />
       <Tiles title={"What I want to learn next 🚀"} data={targetList} />
+      <Portfolio />
       <Footer />
     </StyledPage>
   );
