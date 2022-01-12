e010f47a02dfd11fb6aeb480b833d6203aba94c5
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-12 19:11:39 +0100

    Improved styles added

--- src/common/Tiles/styled.js
+++ src/common/Tiles/styled.js
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
 export const Tile = styled.div`
+    padding: 32px;
+    margin-bottom: 72px;
     background-color: ${({ theme }) => theme.color.white};
+    box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+    border-radius: 5px;
 `;
 
 export const Title = styled.h1`
+    margin: 0px;
     color: ${({ theme }) => theme.color.black};
     font-weight: 900;
     font-size: 30px;
@@ -24,5 +29,5 @@ export const ListItem = styled.li`
 `;
 
 export const Line = styled.span`
-    color: ${({ theme }) => theme.color.black};
+    color: ${({ theme }) => theme.color.slateGray};
 `;
