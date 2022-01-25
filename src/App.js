7c55553600c42c1b779d246425bc8f59b8b38bfe
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-25 18:11:50 +0100

    add Skills and Goals

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import Footer from "./common/Footer";
 import Greeting from "./common/Greeting";
 import Portfolio from "./common/Portfolio";
-import Tiles from "./common/Tiles";
-import skillList from "./common/Tiles/Data/skillList";
-import targetList from "./common/Tiles/Data/targetList";
+import Goals from "./common/Tiles/Goals";
+import Skills from "./common/Tiles/Skills";
 import { StyledPage } from "./styled";
 
 function App() {
   return (
     <StyledPage>
       <Greeting />
-      <Tiles title={"My skillset includes 🛠️"} data={skillList} />
-      <Tiles title={"What I want to learn next 🚀"} data={targetList} />
+      <Skills />
+      <Goals />
       <Portfolio />
       <Footer />
     </StyledPage>
