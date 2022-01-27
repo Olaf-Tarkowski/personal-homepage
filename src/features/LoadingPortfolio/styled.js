72a3cc822ca10df9998bf1839e883b8ec35bfff7
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-27 23:58:38 +0100

    create styling for tiles in portfolio

--- src/features/LoadingPortfolio/styled.js
+++ src/features/LoadingPortfolio/styled.js
@@ -1,5 +1,13 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-export const Section = styled.section`
+export const Section = styled.section``;
 
+export const StyledPortfolioTile = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 32px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    grid-template-columns: 1fr;
+  }
 `;
