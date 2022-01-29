5393b59f2c44f32c2ea8928fd3e348c1835ca6a6
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-30 00:57:08 +0100

    build new theme for background change button

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,96 @@
-export const theme = {
-    color: {
-        white: "#FFFFFF",
-        whiteLilac: "#FBFBFE",
-        black: "#000000",
-        indigo: "#3C6EC9",
-        indigoTransp:"#3c6ec942",
-        slateGray: "#6E7E91",
-        mischka: "#D1D5DA4D",
-        vinRouge: "#8E3C6B",
-        vinRougeLessSaturated: "#8E3C6B9D"
+const colors = {
+  white: "#FFFFFF",
+  whiteTransp: "#FFFFFFC4",
+  whiteLilac: "#FBFBFE",
+  black: "#000000",
+  mineShaft: "#252525",
+  mineShaftTransp: "#65656536",
+  indigo: "#3C6EC9",
+  indigoTransp: "#3c6ec942",
+  slateGray: "#6E7E91",
+  mischka: "#D1D5DA4D",
+  vinRouge: "#8E3C6B",
+  vinRougeLessSaturated: "#8E3C6B9D",
+};
+
+const common = {
+  breakpoint: {
+    mobileMin: "380px",
+    mobileMid: "480px",
+    mobileMax: "767px",
+    tabletMax: "860px",
+    navMax: "1100px",
+  },
+};
+
+export const themeLight = {
+  ...common,
+  color: {
+    boxBackground: colors.white,
+    headerLine: colors.black,
+    site: {
+      background: colors.whiteLilac,
+      text: colors.slateGray,
     },
-    breakpoint: {
-        mobileMin: "380px",
-        mobileMid: "480px",
-        mobileMax: "767px",
-        tabletMax: "860px",
-        navMax: "1100px",
-      },
-}
+    buttonLink: {
+      background: colors.indigo,
+      text: colors.white,
+      hover: colors.vinRouge,
+      active: colors.vinRougeLessSaturated,
+    },
+    list: {
+      headerText: colors.indigo,
+      marker: colors.indigo,
+      line: colors.mischka,
+      text: colors.slateGray,
+      underscore: colors.indigoTransp,
+      activeText: colors.vinRouge,
+      activeUnderscore: colors.vinRougeLessSaturated,
+    },
+    icon: {
+      static: colors.black,
+      hover: colors.vinRouge,
+      active: colors.vinRougeLessSaturated,
+    },
+    themeSwitch: {
+      background: colors.whiteLilac,
+      icon: colors.white,
+    },
+  },
+};
+
+export const themeDark = {
+  ...common,
+  color: {
+    boxBackground: colors.mineShaftTransp,
+    headerLine: colors.white,
+    site: {
+      background: colors.mineShaft,
+      text: colors.whiteTransp,
+    },
+    buttonLink: {
+      background: colors.indigo,
+      text: colors.white,
+      hover: colors.vinRouge,
+      active: colors.vinRougeLessSaturated,
+    },
+    list: {
+      headerText: colors.white,
+      marker: colors.indigo,
+      line: colors.mischka,
+      text: colors.whiteTransp,
+      underscore: colors.indigoTransp,
+      activeText: colors.vinRouge,
+      activeUnderscore: colors.vinRougeLessSaturated,
+    },
+    icon: {
+      static: colors.whiteTransp,
+      hover: colors.vinRouge,
+      active: colors.vinRougeLessSaturated,
+    },
+    themeSwitch: {
+      background: colors.whiteLilac,
+      icon: colors.white,
+    },
+  },
+};
