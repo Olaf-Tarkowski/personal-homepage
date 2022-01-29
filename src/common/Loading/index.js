405c437a03440c26ee081fa908a6daf4985b8d46
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-29 13:09:43 +0100

    create Loading

---
+++ src/common/Loading/index.js
@@ -0,0 +1,13 @@
+import { LoadingImage, LoadingSection } from "./styled";
+
+const Loading = () => {
+  return (
+    <>
+      <LoadingSection>
+        <LoadingImage />
+      </LoadingSection>
+    </>
+  );
+};
+
+export default Loading;
