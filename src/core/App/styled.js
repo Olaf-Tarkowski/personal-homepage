a29e9298da50ed8b7a3a10feaf18c0ff4c14c686
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-30 15:43:54 +0100

    add @media margin

--- src/core/App/styled.js
+++ src/core/App/styled.js
@@ -4,5 +4,9 @@ export const StyledPage = styled.div`
     margin:115px auto;
     padding: 16px;
     max-width: 1244px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+        margin:0px auto;
+  }
 `;
 
