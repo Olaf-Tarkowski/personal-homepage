ed80371a3ce61762f0f1e0259623b10f7269e6a9
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-30 01:01:09 +0100

    Build switchButton

---
+++ src/common/ThemeSwitch/index.js
@@ -0,0 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Box, Button, IconWraper, Section, SunIcon, Text } from "./styled";
+import { selectToggleTheme, toggleTheme } from "./themeSlice";
+
+const ThemeSwitch = () => {
+  const isDarkTheme = useSelector(selectToggleTheme);
+  const dispatch = useDispatch();
+
+  return(
+    <Section>
+      <Button onClick={() => dispatch(toggleTheme())}>
+        <Text> Dark mode {isDarkTheme ? "on" : "off"}</Text>
+        <Box>
+          <IconWraper moveToRight={isDarkTheme}>
+            <SunIcon />
+          </IconWraper>
+        </Box>
+      </Button>
+    </Section>
+  )
+};
+
+export default ThemeSwitch;
