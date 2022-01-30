4e5aae784553526d376997cfa3ec22d19d69846f
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-30 01:03:29 +0100

    initial commit

---
+++ src/core/index.js
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
+import {themeDark, themeLight} from "../theme"
+import PersonalHomepage from "./App";
+import { useSelector } from "react-redux";
+import { selectToggleTheme } from "../common/ThemeSwitch/themeSlice";
+
+const App = () => {
+  const isDarkTheme = useSelector(selectToggleTheme)
+
+  return (
+    <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+      <GlobalStyle />
+      <PersonalHomepage />
+    </ThemeProvider>
+  );
+};
+
+export default App;
