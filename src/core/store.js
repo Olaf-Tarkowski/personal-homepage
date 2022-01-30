b3d65590a0d8115a4a1f12885dab8f2a90ffe865
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-30 01:02:06 +0100

    Add themeReducer to store

--- src/core/store.js
+++ src/core/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import saga from "./saga";
 import personalHomepageReducer from "../features/personalHomepageSlice";
+import themeReducer from "../common/ThemeSwitch/themeSlice"
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         personalHomepage: personalHomepageReducer,
+        theme: themeReducer,
     },
     middleware: [sagaMiddleware],
 });
